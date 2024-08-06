585ab117f87d0d3b274b4ea1d8f5dd07fa9b72c3
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-08-06 15:51:47 +0300

    UDP support

--- etc/ndm/netfilter.d/100-tpws.sh
+++ etc/ndm/netfilter.d/100-tpws.sh
@@ -12,5 +12,6 @@ if [ -z "$(iptables-save 2>/dev/null | grep $BIND_PORT)" ]; then
   for IFACE in $LOCAL_INTERFACE; do
     iptables -t nat -A PREROUTING -i $IFACE -p tcp --dport 80 -j REDIRECT --to-port $BIND_PORT
     iptables -t nat -A PREROUTING -i $IFACE -p tcp --dport 443 -j REDIRECT --to-port $BIND_PORT
+    iptables -t nat -A PREROUTING -i $IFACE -p udp --dport 443 -j REDIRECT --to-port $BIND_PORT
   done
 fi
